558fbac9a0289e42f10e0fe0cf68ca029d774d5c
Siddhesh Gajare <ssgajare12345@gmail.com>
2025-09-24 23:15:11 +0530

    Constructor Overloading2.cpp

---
+++ Constructor Overloading2.cpp
@@ -0,0 +1,37 @@
+// Exp - 13
+// Siddhesh
+// 25070123505
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Name 
+{
+private:
+double sum;
+public:
+   
+        Name(char A, char B) {
+        cout<<A;
+        cout<<B<<endl;
+    }
+
+
+       Name(string a, string b) {
+        cout  << a+b << endl;
+    }
+};
+    
+int main() {
+    Name A1('A','B');  
+    Name A2("Siddhesh","Gajare");                 
+    return 0;
+}
+
+Output :-
+AB
+SiddheshGajare
+
+
+=== Code Execution Successful ===
